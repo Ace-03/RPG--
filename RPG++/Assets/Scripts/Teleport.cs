1794e9b4685a7c346308932d50b66eb261554fa5
Ace-03 <66760646+Ace-03@users.noreply.github.com>
2023-10-27 10:23:15 -0700

    improved teleporters

--- RPG++/Assets/Scripts/Teleport.cs
+++ RPG++/Assets/Scripts/Teleport.cs
@@ -5,27 +5,15 @@ using Photon.Pun;
 using Photon.Realtime;
 public class Teleport : MonoBehaviourPun
 {
-    [Header("Info")]
-    public Transform teleporterTarget;
-    public float teleportBuffer;
-
     [Header("Components")]
     public PlayerController player;
-
-    private float lastTeleport;
+    public Transform teleporterTarget;
 
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-
-
-        if (Time.time - lastTeleport < teleportBuffer)
-            return;
-
-        lastTeleport = Time.time;
-
         Debug.Log("Player Touched the teleporter");
         player = GameManager.instance.GetPlayer(collision.gameObject);
-        player.transform.position = teleporterTarget.transform.position - new Vector3(2,0,0);
+        player.transform.position = teleporterTarget.transform.position;
     }
 }
